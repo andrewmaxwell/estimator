f95760799baa3a31beac672dd69e80dd62f37ecf
Andrew Maxwell <emailandrewmaxwell@gmail.com>
2024-04-30 16:46:16 -0500

    blank project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import {Box, Container} from '@mui/material';
+
+export const App = () => (
+  <Box sx={{flexGrow: 1}}>
+    <Container sx={{pb: 10}} maxWidth="xl">
+      <h1>Hello World!</h1>
+    </Container>
+  </Box>
+);
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import {App} from './App.tsx';
+import {createTheme, ThemeProvider, CssBaseline} from '@mui/material';
+
+const darkTheme = createTheme({palette: {mode: 'dark'}});
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </StrictMode>,
+);
